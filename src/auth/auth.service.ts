3f238edec47d4864156ecc06dc7677dd07467220
khalvai <khalvayozbek@gmail.com>
2023-07-07 11:55:49 +0330

    add : channel module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,17 @@
-import { BadRequestException, Inject, Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  HttpException,
+  Inject,
+  Injectable,
+} from "@nestjs/common";
 import { SignupInput } from "./dto/signup.dto";
-import { UpdateAuthInput } from "./dto/update-auth.input";
 import { Collection, Db } from "mongodb";
 import { JwtService } from "@nestjs/jwt";
 import { JwtPayload } from "./jwt.payload";
 import { ConfigService } from "@nestjs/config";
 import { AuthenticatedPayload } from "./entities/auth.entity";
 import * as argon2 from "argon2";
+import { LoginInput } from "./dto/login.dto";
 
 @Injectable()
 export class AuthService {
@@ -19,14 +24,24 @@ export class AuthService {
     this.collection = db.collection("users");
   }
 
-  async create(signUpDto: SignupInput): Promise<AuthenticatedPayload> {
+  async signup(signUpDto: SignupInput): Promise<AuthenticatedPayload> {
+    const userExists = await this.findByEmail(signUpDto.email);
+
+    if (userExists) {
+
+      throw new HttpException("an account exists with this email", 404);
+    }
+
     const userData = {
       name: signUpDto.name,
       email: signUpDto.email,
-      password: argon2.hash(signUpDto.password),
+      password: await argon2.hash(signUpDto.password),
     };
     const user = await this.collection.insertOne(userData);
-    const token = this.getToken({ sub: user.insertedId, name: signUpDto.name });
+    const token = this.getToken({
+      sub: user.insertedId,
+      name: signUpDto.name,
+    });
 
     return { name: signUpDto.name, token };
   }
@@ -35,18 +50,44 @@ export class AuthService {
     return this.jwtService.sign(jwtPayload);
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async findByEmail(email: string) {
+    return await this.collection.findOne(
+      { email: email },
+      { projection: { password: 0 } }
+    );
+  }
+
+  async login(loginDto: LoginInput): Promise<AuthenticatedPayload> {
+    const user = await this.findByEmailOrThrow(loginDto.email);
+
+    const isValid = await argon2.verify(user.password, loginDto.password);
+
+    if (!isValid) {
+      throw new BadRequestException("the credentials aren't correct");
+    }
+
+    return {
+      token: this.getToken({ sub: user._id, name: user.name }),
+      name: user.name,
+    };
   }
 
   async findByEmailOrThrow(email: string) {
-    const user = await this.collection.findOne({ email: email });
+    const options = {
+      projection: { email: 1, password: 1, name: 1, _id: 1 },
+    };
+
+    const user = await this.collection.findOne(
+      {
+        email: email,
+      },
+      options
+    );
 
     if (!user) {
       throw new BadRequestException("no");
     }
+
     return user;
   }
-
-  // async login ( )
 }
